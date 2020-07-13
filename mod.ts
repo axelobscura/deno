8701e2e15c7d6a3a3f1a5c880374775de21a0e82
Axel Obscura <axosar@gmail.com>
2020-07-13 16:24:52 -0500

    Launch Poject X

--- mod.ts
+++ mod.ts
@@ -1,49 +1,18 @@
-import { join } from "https://deno.land/std/path/mod.ts";
-import { BufReader } from "https://deno.land/std/io/bufio.ts";
-import { parse } from "https://deno.land/std/encoding/csv.ts";
+import * as log from "https://deno.land/std/log/mod.ts";
 
-import * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
-
-interface Planet {
-  [key: string]: string;
-}
-
-async function loadPlanetsData() {
-  const path = join(".", "kepler_exoplanets_nasa.csv");
-
-  const file = await Deno.open(path);
-  const bufReader = new BufReader(file);
-  const result = await parse(bufReader, {
-    header: true,
-    comment: "#",
-  });
-  Deno.close(file.rid);
-
-  const planets = (result as Array<Planet>).filter((planet) => {
-    const planetaryRadius = Number(planet["koi_prad"]);
-    const stellarMass = Number(planet["koi_smass"]);
-    const stellarRadius = Number(planet["koi_srad"]);
-
-    return planet["koi_disposition"] === "CONFIRMED" &&
-      planetaryRadius > 0.5 && planetaryRadius < 1.5 &&
-      stellarMass > 0.78 && stellarMass < 1.04 &&
-      stellarRadius > 0.80 && stellarRadius < 1.01;
+async function downloadLaunchData() {
+  log.info("Downloading launch data....");
+  const response = await fetch("https://api.spacexdata.com/v3/launches", {
+    method: "GET",
   });
 
-  return planets.map((planet) => {
-    return _.pick(planet, [
-      "koi_prad",
-      "koi_smass",
-      "koi_srad",
-      "kepler_name",
-      "koi_count",
-      "koi_steff",
-    ]);
-  });
+  if (!response.ok) {
+    log.warning("Problem downlaoding launch data...");
+    //throw new Error("Launch data failed...");
+  }
+
+  const launchData = await response.json();
+  console.log(launchData);
 }
 
-const newEarths = await loadPlanetsData();
-for (const planet of newEarths) {
-  console.log(planet);
-}
-console.log(`${newEarths.length} habitable planets...`);
+await downloadLaunchData();
