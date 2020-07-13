704618105a7b7541cf91dca876e311d9ef40d3c2
Axel Obscura <axosar@gmail.com>
2020-07-13 09:46:11 -0500

    Exploring planets

--- mod.ts
+++ mod.ts
@@ -2,6 +2,8 @@ import { join } from "https://deno.land/std/path/mod.ts";
 import { BufReader } from "https://deno.land/std/io/bufio.ts";
 import { parse } from "https://deno.land/std/encoding/csv.ts";
 
+import * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
+
 interface Planet {
   [key: string]: string;
 }
@@ -22,14 +24,26 @@ async function loadPlanetsData() {
     const stellarMass = Number(planet["koi_smass"]);
     const stellarRadius = Number(planet["koi_srad"]);
 
-    return planet["koi_disposition;"] === "CONFIRMED" &&
+    return planet["koi_disposition"] === "CONFIRMED" &&
       planetaryRadius > 0.5 && planetaryRadius < 1.5 &&
       stellarMass > 0.78 && stellarMass < 1.04 &&
-      stellarRadius > 0.99 && stellarRadius < 1.01;
+      stellarRadius > 0.80 && stellarRadius < 1.01;
   });
 
-  return planets;
+  return planets.map((planet) => {
+    return _.pick(planet, [
+      "koi_prad",
+      "koi_smass",
+      "koi_srad",
+      "kepler_name",
+      "koi_count",
+      "koi_steff",
+    ]);
+  });
 }
 
 const newEarths = await loadPlanetsData();
+for (const planet of newEarths) {
+  console.log(planet);
+}
 console.log(`${newEarths.length} habitable planets...`);
