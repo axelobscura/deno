8f203265ddd323dcfb06e08e7a31d237ca8f9747
Axel Obscura <axosar@gmail.com>
2020-07-14 08:43:24 -0500

    Exploring Space X Launch data

--- mod.ts
+++ mod.ts
@@ -1,5 +1,12 @@
 import * as log from "https://deno.land/std/log/mod.ts";
 
+interface Launch {
+  flightNumber: number;
+  mission: string;
+}
+
+const launches = new Map<number, Launch>();
+
 async function downloadLaunchData() {
   log.info("Downloading launch data....");
   const response = await fetch("https://api.spacexdata.com/v3/launches", {
@@ -12,7 +19,15 @@ async function downloadLaunchData() {
   }
 
   const launchData = await response.json();
-  console.log(launchData);
+  for (const launch of launchData) {
+    const flightData = {
+      flightNumber: launch["flight_number"],
+      mission: launch["mission_name"],
+    };
+    launches.set(flightData.flightNumber, flightData.mission);
+    log.info(JSON.stringify(flightData));
+  }
+  //console.log(launchData);
 }
 
 await downloadLaunchData();
